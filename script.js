031e875f50967f6a977ea4e55728f52f4ad50866
Gabriel Vieira <112446183+devgabrielvieira@users.noreply.github.com>
2025-04-11 23:49:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,31 +33,3 @@ window.addEventListener('scroll', function () {
 backToTopButton.addEventListener('click', function () {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
-
-// Seleciona todas as imagens dos projetos
-document.querySelectorAll('.project-item img').forEach(img => {
-    img.addEventListener('click', function () {
-        // Cria o lightbox e o conteúdo da imagem
-        const lightbox = document.createElement('div');
-        lightbox.id = 'lightbox';
-        lightbox.innerHTML = `
-            <div id="lightbox-content">
-                <img src="${img.src}" alt="${img.alt}">
-                <button id="close-lightbox">Fechar</button>
-            </div>
-        `;
-        document.body.appendChild(lightbox);
-        
-        // Adiciona a funcionalidade de fechar ao clicar fora da imagem
-        lightbox.addEventListener('click', (e) => {
-            if (e.target === lightbox) {
-                lightbox.remove(); // Fecha o lightbox ao clicar fora da imagem
-            }
-        });
-        
-        // Fecha o lightbox ao clicar no botão de fechar
-        document.getElementById('close-lightbox').addEventListener('click', () => {
-            lightbox.remove();
-        });
-    });
-});
