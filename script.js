3950416dca1db30f5d60148db227b66a69864321
Gabriel Vieira <112446183+devgabrielvieira@users.noreply.github.com>
2025-04-11 23:54:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 // Animação ao rolar para tornar as seções visíveis
 window.addEventListener('scroll', function () {
-    const sections = document.querySelectorAll('section');
+    const sections = document.querySelectorAll('section'); // ajustado para selecionar diretamente todas as seções
     sections.forEach(section => {
         const position = section.getBoundingClientRect().top;
-        if (position < window.innerHeight - 100) { // Ajustado para um efeito mais suave
+        if (position < window.innerHeight) {
             section.classList.add('visible');
         }
     });
@@ -22,14 +22,33 @@ window.addEventListener('scroll', function () {
 // Botão "Topo"
 const backToTopButton = document.getElementById('backToTop');
 
-window.addEventListener('scroll', function () {
+window.onscroll = function () {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
         backToTopButton.style.display = "block";
     } else {
         backToTopButton.style.display = "none";
     }
-});
+};
 
 backToTopButton.addEventListener('click', function () {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// Seleciona todas as imagens dos projetos
+document.querySelectorAll('.project-item img').forEach(img => {
+    img.addEventListener('click', function() {
+        // Cria o lightbox e o conteúdo da imagem
+        const lightbox = document.createElement('div');
+        lightbox.id = 'lightbox';
+        lightbox.innerHTML = `<img src="${img.src}" alt="${img.alt}">`;
+        document.body.appendChild(lightbox);
+        lightbox.addEventListener('click', () => {
+            lightbox.remove(); // Fecha o lightbox ao clicar fora da imagem
+        });
+    });
+});
+
+// Fecha o lightbox ao clicar no botão de fechar
+document.getElementById('close-lightbox').addEventListener('click', () => {
+    document.getElementById('lightbox').remove();
 });
